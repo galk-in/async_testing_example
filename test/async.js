0c1a03a534fb694c421d7ddc3b44c55be120116a
Nikita Galkin <nikita@galk.in>
2016-03-25 13:27:19 +0200

    Refactoring

--- test/async.js
+++ test/async.js
@@ -3,7 +3,7 @@
 let request = require('supertest');
 
 describe('Test async', () => {
-    let url = 'http://localhost:3000/checkCMS';
+    let url = 'http://localhost:3000/checkCMS/';
     let callbackApp = require('express')();
     let server;
     callbackApp.use(require('body-parser').json());
@@ -15,35 +15,51 @@ describe('Test async', () => {
         res.status(200).end();
     });
 
-    server = callbackApp.listen(8000, '172.28.23.92');
+    server = callbackApp.listen(8000, '192.168.99.1');
 
     after(() => server.close());
 
     it('Should return unknown CMS for mail.ru', (done) => {
-        callback = (body) => {
-            if (body.cms === 'unknown') done();
+        let domian = 'mail.ru';
+        let cms = 'unknown';
+
+        request(url).get(domian).end(() => {});
+
+        callback = (log) => {
+            if (log.cms === cms) done();
         };
-        request(url).get('/mail.ru').end(() => {});
     });
 
     it('Should return wordpress CMS for lifehacker.ru', (done) => {
-        callback = (body) => {
-            if (body.cms === 'wordpress') done();
+        let domian = 'lifehacker.ru';
+        let cms = 'wordpress';
+
+        request(url).get(domian).end(() => {});
+
+        callback = (log) => {
+            if (log.cms === cms) done();
         };
-        request(url).get('/lifehacker.ru').end(() => {});
     });
 
     it('Should return joomla CMS for joomla.ru', (done) => {
-        callback = (body) => {
-            if (body.cms === 'joomla') done();
+        let domian = 'joomla.ru';
+        let cms = 'joomla';
+
+        request(url).get(domian).end(() => {});
+
+        callback = (log) => {
+            if (log.cms === cms) done();
         };
-        request(url).get('/joomla.ru').end(() => {});
     });
 
     it('Should return error for 88888', (done) => {
-        callback = (body) => {
-            if (body.message === 'Error during request') done();
+        let domian = '88888';
+        let message = 'Error during request';
+
+        request(url).get(domian).end(() => {});
+
+        callback = (log) => {
+            if (log.message === message) done();
         };
-        request(url).get('/88888').end(() => {});
     });
 });
